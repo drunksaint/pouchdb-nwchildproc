df79e589d3ca1c7b55bdedc00336652c1f32cc54
Saurabh Agrawal <drunksaint@gmail.com>
2016-08-08 21:15:14 +0530

    added messages to debug on os's other than linux

--- cp.js
+++ cp.js
@@ -1,6 +1,12 @@
 var PouchDB = require('pouchdb');
+// var pdb = new PouchDB('testlev');
 require('pouchdb/extras/websql');
 var pdb = new PouchDB('test', {adapter: 'websql'});
 pdb.info().then(function(r){
     console.log(r);
-})
+    process.send(r);
+});
+process.on('message', function (m) {
+    console.log('received message from parent: ', m);
+    process.send(m);
+});
